d19ade578096240e83dd7fe868f1c9a47ba4603b
oDs1337 <odstobiaszcod@gmail.com>
2022-07-11 18:16:38 +0200

    Add database with e-bikes and e-bike interface

---
+++ src/app/database/ebikes.ts
@@ -0,0 +1,39 @@
+import { Ebike } from "../ebike";
+
+export const ebikes: Ebike[] = [
+  {
+    image: "https://www.trinitycyclesindia.com/wp-content/uploads/2022/04/SCHNELL-METTA-FAT-BIKE-150x150.jpg",
+    name: "Green Bike",
+    description: "bike with green color",
+    price: 1350,
+    currency: "EUR",
+  },
+  {
+    image: "https://gilickibike.pl/7285-home_default/ROWER-M-BIKE-T-BIKE-9-2-LADY-L-WHITE-PINK.jpg",
+    name: "White Bike",
+    description: "bike with white color",
+    price: 1550,
+    currency: "EUR",
+  },
+  {
+    image: "https://peleton.pl/wp-content/uploads/2021/07/ROWER-KROSS-29%E2%80%B3-M-LEVEL-1.0-S-cz-150x150.jpg",
+    name: "Yellow Bike",
+    description: "bike with yellow color",
+    price: 7525,
+    currency: "EUR",
+  },
+  {
+    image: "https://bukbike.pl/wp-content/uploads/2021/07/Qki-16-niebieski-150x150.png",
+    name: "Blue Bike",
+    description: "bike with blue color",
+    price: 7525,
+    currency: "EUR",
+  },
+  {
+    image: "https://bukbike.pl/wp-content/uploads/2021/07/Qki-roz-150x150.png",
+    name: "Pink Bike",
+    description: "bike with pink color",
+    price: 7525,
+    currency: "EUR",
+  }
+]
---
+++ src/app/ebike.ts
@@ -0,0 +1,7 @@
+export interface Ebike {
+  image: string,
+  name: string,
+  description: string,
+  price: number,
+  currency: string,
+}
