64d964015594e31c1cd7514409b824d5e23be715
oDs1337 <odstobiaszcod@gmail.com>
2022-07-14 20:05:00 +0200

    Change reducer to more efficient version

--- src/app/state/ebike.reducer.ts
+++ src/app/state/ebike.reducer.ts
@@ -7,17 +7,17 @@ export const initialState: Ebike[] = [];
 export const counterReducer = createReducer(
   initialState,
   on(increment, (state: Ebike[], action) => {
-    const newState = JSON.parse(JSON.stringify(state));
+    const newState = state.map(item => ({...item}));
     newState[action.index].count += 1;
     return newState;
   }),
   on(decrement, (state: Ebike[], action) => {
-    const newState = JSON.parse(JSON.stringify(state));
+    const newState = state.map(item => ({...item}));
     newState[action.index].count -= 1;
     return newState;
   }),
   on(remove, (state, action) => {
-    const newState = JSON.parse(JSON.stringify(state));
+    const newState = state.map(item => ({...item}));
     newState[action.index].count = 0;
     return newState;
   }),
